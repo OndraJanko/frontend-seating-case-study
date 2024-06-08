841d3905dec28c82180ede34ebccff274ff30291
Ondra Janko <ondrajanko13@gmail.com>
2024-06-08 13:46:02 +0200

    style: added gradient bg to EventSeats

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import PriceFooter from "@/components/PriceFooter";
 
 function Event() {
   return (
-    <div className="flex w-full flex-col gap-5 overflow-auto px-5 py-8 md:flex-row lg:px-32">
+    <div className="flex w-full flex-col gap-5 px-5 py-8 md:flex-row lg:px-32">
       <EventDetails />
-      <EventSeats />
+      <div className="relative flex h-full w-full md:w-2/3">
+        <EventSeats />
+        <div className="absolute bottom-[-5px] right-[-5px] z-0 h-full w-full rounded-md bg-gradient-to-bl from-gradient-start via-gradient-middle to-gradient-end md:bottom-[-10px] md:right-[-10px] lg:bottom-[-20px] lg:right-[-20px]"></div>
+      </div>
     </div>
   );
 }
--- components/EventSeats.tsx
+++ components/EventSeats.tsx
@@ -2,7 +2,7 @@ import Seat from "./Seat";
 
 export default function EventSeats() {
   return (
-    <div className="glass_bg grid h-full w-full grid-cols-12 gap-x-1 gap-y-5 rounded-md px-7 py-9 pb-24 md:w-2/3">
+    <div className="glass_bg relative z-[1] grid h-full w-full grid-cols-12 gap-x-1 gap-y-5 rounded-md px-7 py-9 pb-24">
       <Seat />
       <Seat />
       <Seat />
