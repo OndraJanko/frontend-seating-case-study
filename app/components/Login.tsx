7dc708684e3be4edc957edecef9ab904022676c0
Ondra Janko <ondrajanko13@gmail.com>
2024-06-08 13:18:49 +0200

    refactor: remove unused import from Login.tsx

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -13,7 +13,6 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { CiMenuBurger } from "react-icons/ci";
 
 export default function Login() {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
