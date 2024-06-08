b720cc9ff34df647c518b48a312d5d9accdb76bc
Ondra Janko <ondrajanko13@gmail.com>
2024-06-08 08:50:18 +0200

    refactor: restructure project to Next.js, added Prettier plugin and Shadcn UI library

---
+++ components/Seat.tsx
@@ -0,0 +1,7 @@
+export default function Seat() {
+  return (
+    <div className="transition-color size-8 rounded-full bg-zinc-100 hover:bg-zinc-200">
+      <span className="text-xs font-medium text-zinc-400">[n]</span>
+    </div>
+  );
+}
