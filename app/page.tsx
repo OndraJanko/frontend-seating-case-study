3529e9da32103457982da350c47e95076d3cb4c0
Ondra Janko <ondrajanko13@gmail.com>
2024-06-08 21:36:45 +0200

    fix: removed redundant query provider from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import EventContainer from "@/components/EventContainer";
 import PriceFooter from "@/components/PriceFooter";
-import { fetchEvent } from "@/lib/fetchers";
-import QueryProvider from "./_components/queryprovider";
 
 export default async function Home() {
   return (
-    <QueryProvider>
-      <main className="flex h-full flex-col items-center justify-start">
-        <EventContainer />
-        <PriceFooter />
-      </main>
-    </QueryProvider>
+    <main className="flex h-full flex-col items-center justify-start">
+      <EventContainer />
+      <PriceFooter />
+    </main>
   );
 }
