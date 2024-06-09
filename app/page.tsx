263584ac7fe9ad53a8aa8c4a978bcf75680b6213
Ondra Janko <ondrajanko13@gmail.com>
2024-06-09 09:23:21 +0200

    refactor:removed react icons dependency style: added different gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ export default async function Home() {
     <main className="flex w-full flex-col gap-5 px-5 py-8 md:flex-row lg:px-20">
       <div className="relative flex h-full w-full md:w-1/3">
         <EventDetails />
-        <div className="absolute left-[-5px] top-[-5px] z-0 h-full w-full rounded-md bg-gradient-to-bl from-gradient-start via-gradient-middle to-gradient-end md:left-[-10px] md:top-[-10px] lg:left-[-20px] lg:top-[-20px]" />
+        <div className="gradient_bg absolute left-[-5px] top-[-5px] z-0 h-full w-full rounded-md md:left-[-10px] md:top-[-10px] lg:left-[-20px] lg:top-[-20px]" />
       </div>
       <div className="relative flex h-full w-full flex-col gap-10 md:w-2/3">
         <div className="relative flex h-full w-full flex-col">
           <EventSeats />
-          <div className="absolute bottom-[-5px] right-[-5px] z-0 h-full w-full rounded-md bg-gradient-to-bl from-gradient-start via-gradient-middle to-gradient-end md:bottom-[-10px] md:right-[-10px] lg:bottom-[-20px] lg:right-[-20px]" />
+          <div className="gradient_bg absolute bottom-[-5px] right-[-5px] z-0 h-full w-full rounded-md md:bottom-[-10px] md:right-[-10px] lg:bottom-[-20px] lg:right-[-20px]" />
         </div>
         <TicketCheckout />
         <div className="flex items-center justify-center md:hidden">
