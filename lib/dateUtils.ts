e163ed6ab9fe9a341335b70aa61b100b9e56bc90
Ondra Janko <ondrajanko13@gmail.com>
2024-06-10 21:56:09 +0200

    fix: add luxon for datetime formatting hydration error

--- lib/dateUtils.ts
+++ lib/dateUtils.ts
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 function isValidDateFormat(date: string): boolean {
   const dateFormatRegex =
     /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}(Z|[+-]\d{2}:\d{2})$/;
@@ -14,20 +16,20 @@ export function formatDateRange(dateFrom: string, dateTo: string): string {
     return "N/A";
   }
 
-  const fromDate = new Date(dateFrom);
-  const toDate = new Date(dateTo);
+  const fromDate = DateTime.fromISO(dateFrom).setZone("utc");
+  const toDate = DateTime.fromISO(dateTo).setZone("utc");
 
-  const dayFrom = String(fromDate.getDate()).padStart(2, "0");
-  const monthFrom = String(fromDate.getMonth() + 1).padStart(2, "0");
-  const yearFrom = fromDate.getFullYear();
-  const hoursFrom = String(fromDate.getHours()).padStart(2, "0");
-  const minutesFrom = String(fromDate.getMinutes()).padStart(2, "0");
+  const dayFrom = String(fromDate.day).padStart(2, "0");
+  const monthFrom = String(fromDate.month).padStart(2, "0");
+  const yearFrom = fromDate.year;
+  const hoursFrom = String(fromDate.hour).padStart(2, "0");
+  const minutesFrom = String(fromDate.minute).padStart(2, "0");
 
-  const dayTo = String(toDate.getDate()).padStart(2, "0");
-  const monthTo = String(toDate.getMonth() + 1).padStart(2, "0");
-  const yearTo = toDate.getFullYear();
-  const hoursTo = String(toDate.getHours()).padStart(2, "0");
-  const minutesTo = String(toDate.getMinutes()).padStart(2, "0");
+  const dayTo = String(toDate.day).padStart(2, "0");
+  const monthTo = String(toDate.month).padStart(2, "0");
+  const yearTo = toDate.year;
+  const hoursTo = String(toDate.hour).padStart(2, "0");
+  const minutesTo = String(toDate.minute).padStart(2, "0");
 
   if (yearFrom === yearTo && monthFrom === monthTo && dayFrom === dayTo) {
     // Same day
